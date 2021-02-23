a21c867e9527613788e83b1dd2dcf80cbeed722d
Lavanya Santhosh <49737012+lav30@users.noreply.github.com>
2021-02-23 13:21:42 -0800

    Update nobel_prize.py

--- nobel_prize.py
+++ nobel_prize.py
@@ -10,7 +10,7 @@ from dash.dependencies import Input, Output
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
-
+server = app.server
 
 # Import and clean data (importing csv into pandas)
 df = pd.read_csv("archive.csv")
